60dbcf21d2f481b7d6b0627a637c0c96c0d2f101
Roman Shauk <shauk60@gmail.com>
2019-04-30 12:26:58 +0300

    Updates

---
+++ precache-manifest.57025ea6f1c6b5cff036515a8c9aaf26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23792e26392bcad13576",
+    "url": "/nodeapp/static/js/runtime~main.c2bcae56.js"
+  },
+  {
+    "revision": "3a8751425e27609902d8",
+    "url": "/nodeapp/static/js/main.851ecb7b.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/nodeapp/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3a8751425e27609902d8",
+    "url": "/nodeapp/static/css/main.7b79aa16.chunk.css"
+  },
+  {
+    "revision": "6b18b6818529a7c8db5c325722e3b7ea",
+    "url": "/nodeapp/index.html"
+  }
+];
